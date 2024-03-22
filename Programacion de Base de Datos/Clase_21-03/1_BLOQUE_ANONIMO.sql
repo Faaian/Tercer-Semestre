59808526f1c314e2a0cc2770dd3f18b211009215
unknown <Cetecom@SJAPCA03LC0820.duoc.acad>
2024-03-21 21:35:42 -0300

    nueva clase

--- Programacion de Base de Datos/Clase_21-03/1_BLOQUE_ANONIMO.sql
+++ Programacion de Base de Datos/Clase_21-03/1_BLOQUE_ANONIMO.sql
@@ -1,3 +1,27 @@
 
+/*
+Imprimir el nombre completo de una
+persona por el id.
+*/
+
 DECLARE
-//
+    -- VARIABLES
+    v_id_persona int := :x_id_persona;
+    v_nombre_completo VARCHAR2(100);
+    v_porc_imp int;
+BEGIN
+    -- varible regular
+    v_porc_imp := 10000 * 0.19;
+    
+    -- variable sql
+    SELECT nombre || ' ' || apellido
+    into v_nombre_completo
+    FROM persona
+    WHERE id_persona = v_id_persona;
+    
+    dbms_output.put_line('hola');
+    dbms_output.put_line('El nombre del usuario ' || v_id_persona);
+    dbms_output.put_line('Es: ' || v_nombre_completo);
+    
+    
+END;
